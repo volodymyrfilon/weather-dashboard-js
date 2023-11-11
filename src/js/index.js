d560452305abd70198b4491358a73f52586cb2b6
Volodymyr <volodymyrfilon1312@gmail.com>
2023-11-11 14:39:31 +0100

    header - darkmode

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,13 @@
-// import mobileNav from './modules/mobile-nav.js';
-// mobileNav();
+const darkModeCheck = document.querySelector(".header__darkmode-check");
+const darkModeText = document.querySelector('.header__darkmode-text');
+
+darkModeCheck.addEventListener('change', () => {
+    document.body.classList.toggle('dark');
+    
+    if (document.body.classList.contains('dark')) {
+        darkModeText.textContent = "Dark Mode";
+    } else {
+        darkModeText.textContent = "Light Mode";
+    }
+})
+
